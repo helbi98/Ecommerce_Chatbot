156b017dd2b600df69c3815845be4d3d88c37b94
Helbi Mathew <36873682+helbi98@users.noreply.github.com>
2024-11-27 04:29:22 +0100

    SQL integration

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,23 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.events import FollowupAction
 from typing import Any, Text, Dict, List
 import re
+import random
+import mysql.connector
+from mysql.connector import Error
+
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password",
+            database="ecommerce_bot",
+        )
+        return connection
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
 
 
 # To check if the order number is a valid 5-digit number
@@ -73,17 +90,33 @@ class ActionValidateItem(Action):
     ) -> List[Dict[Text, Any]]:
         available_items = ["smartphone", "headphones", "laptop", "smartwatch"]
         item_name = tracker.get_slot("item_name")
+        order_number = f"{random.randint(10000, 99999)}"
 
-        if item_name and item_name.lower() in available_items:
-            dispatcher.utter_message(
-                text=f"Great choice! Placed an order for {item_name}."
-            )
-            return [SlotSet("item_name", item_name)]
+        connection = get_db_connection()
+        if connection:
+            if item_name and item_name.lower() in available_items:
+                cursor = connection.cursor()
+                insert_query = """ 
+                                INSERT INTO orders (order_number, item_name, order_status)
+                                    VALUES (%s, %s, %s)
+                                """
+                cursor.execute(insert_query, (order_number, item_name, "Placed"))
+                connection.commit()
+                cursor.close()
+                connection.close()
+                dispatcher.utter_message(
+                    text=f"Great choice! Placed an order for {item_name}. Order number: {order_number}."
+                )
+                return [SlotSet("item_name", item_name)]
+            else:
+                dispatcher.utter_message(
+                    text=f"Sorry, this item is not available. Please choose from the below items:\n1. Smartphone\n2. Headphones\n3. Laptop\n4. Smartwatch\n"
+                )
+                return [SlotSet("item_name", None)]
         else:
             dispatcher.utter_message(
-                text=f"Sorry, this item is not available. Please choose from the below items:\n1. Smartphone\n2. Headphones\n3. Laptop\n4. Smartwatch\n"
+                "Sorry, there was an issue placing your order. Please try again later."
             )
-            return [SlotSet("item_name", None)]
 
 
 # Logic for tracking order
@@ -95,10 +128,30 @@ class ActionTrackOrder(Action):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict
     ) -> list:
         order_number = tracker.get_slot("order_number")
-        if order_number:
-            dispatcher.utter_message(text=f"Your order {order_number} is on the way.")
+
+        connection = get_db_connection()
+        if connection:
+            if order_number:
+                cursor = connection.cursor()
+                select_query = "SELECT order_status FROM orders WHERE order_number = %s"
+                cursor.execute(select_query, (order_number,))
+                result = cursor.fetchone()
+
+                if result:
+                    status = result[0]
+                    dispatcher.utter_message(
+                        text=f"Your order {order_number} is {status}."
+                    )
+                else:
+                    dispatcher.utter_message(
+                        text="Sorry, I couldn't find your order number."
+                    )
+                cursor.close()
+                connection.close()
         else:
-            dispatcher.utter_message(text="Sorry, I couldn't find your order number.")
+            dispatcher.utter_message(
+                "Sorry, I couldn't track your order at the moment."
+            )
         return [SlotSet("order_number", None)]
 
 
@@ -111,8 +164,28 @@ class ActionCancelOrder(Action):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict
     ) -> list:
         order_number = tracker.get_slot("order_number")
-        if order_number:
-            dispatcher.utter_message(text=f"Order {order_number} has been cancelled.")
+        connection = get_db_connection()
+        if connection:
+            if order_number:
+                cursor = connection.cursor()
+
+                update_query = (
+                    "UPDATE orders SET order_status = %s WHERE order_number = %s"
+                )
+                cursor.execute(update_query, ("Cancelled", order_number))
+                connection.commit()
+                if cursor.rowcount > 0:
+                    dispatcher.utter_message(
+                        text=f"Order {order_number} has been cancelled."
+                    )
+                else:
+                    dispatcher.utter_message(
+                        text="Sorry, I couldn't find your order number."
+                    )
+                cursor.close()
+                connection.close()
         else:
-            dispatcher.utter_message(text="Sorry, I couldn't find your order number.")
+            dispatcher.utter_message(
+                "Sorry, I couldn't cancel your order at the moment."
+            )
         return [SlotSet("order_number", None)]
